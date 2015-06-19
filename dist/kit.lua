7a472daed83456ae60c6ea7fd27af259ce4383fe
Sander Laarhoven <lesander@users.noreply.github.com>
2015-06-19 17:57:14 +0200

    Improve comments

--- dist/kit.lua
+++ dist/kit.lua
@@ -1,14 +1,22 @@
-  -- Kit v1.3.0 unstable dist. API
-  -- Copyright AxTo Inc. Released under the MIT License.
-  
-  -- Made for ComputerCraft 1.7 and above.
-  
-  -- How to include the API in your file (assuming that this file is called "kit"):
-  -- os.loadAPI("kit")
-  -- Now call a function from this file using:
-  -- kit.functionName()
-  
-  -- Returns Kit version (data response).
+--[[
+    Kit version 1.3.0
+    Copyright AxTo Inc.
+    Released under the MIT License
+    http://git.io/kitdocs
+
+    Made for ComputerCraft 1.7 and above.
+
+    How to include the API in your file (assuming that this file is called "kit"):
+    os.loadAPI("kit")
+    Call a function from this file using:
+    kit.functionName()
+]]--
+
+  --[[
+      Get local or latest Kit version.
+      
+      @returns string
+  ]]--
   function version(getLatest)
     if getLatest then
       return load("https://raw.githubusercontent.com/AxTo/Kit/master/version", true)
@@ -16,8 +24,12 @@
       return "1.3.0"
     end
   end
-  
-  -- Chceks if computer is an advanced computer (boolean response).
+
+  --[[
+      Check if the computer is advanced or not.
+      
+      @returns boolean
+  ]]--
   function isAdvanced()
     if term.isColor() == false then
       return false
@@ -25,8 +37,12 @@
       return true
     end
   end
-  
-  -- Checks all sides for peripherals and wraps them (number of wrapped sides response).
+
+  --[[
+      Wrap all attatched peripherals on the computer.
+      
+      @returns table
+  ]]--
   function wrapSides()
     local peripherals = peripheral.getNames()
     local peripheralTable = {}
@@ -36,11 +52,14 @@
     end
     return peripheralTable
   end
-  
-  -- Load a file or webpage (fetched data respponse).
-  -- Parameters:
-  --  path: the path to the file including the file name (required).
-  --  isurl: if path is an URL. Use 'true' if path is an URL (optional).
+
+  --[[
+      Load a local file or remote web page.
+      
+      @param string path The path to the file including the file name (required)
+      @param boolean isurl If path is an URL. Use 'true' if path is an URL (optional)
+      @returns string / boolean Returns string with data or false on error.
+  ]]--
   function load(path, isurl)
     if path == "" or path == nil then
       error("Invalid arguments for load()", 2)
@@ -64,13 +83,16 @@
     end
   end
   
-  -- Save data to file (boolean response).
-  -- Parameters:
-  --  data: the text/code you want to save (required).
-  --  path: the path to the file including the file name (required).
-  --  method: (required)
-  --    "w" to open it for writing and remove any existing data on file open.
-  --    "a" to open for writing but keep existing data and append any writes to the end of the file.
+  --[[
+      Save given data to local file.
+      
+      @param string data The text or code you want to save (required)
+      @param string path The path to save the file to (required)
+      @param string method The method to use when opening the file, use:
+      "w" to open the file for writing and remove any existing data on file open or;
+      "a" to open for writing but keep existing data and append any writes to the end of the file.
+      @returns boolean
+  ]]--
   function save(data, path, method)
     if data == "" or data == nil or path == "" or path == nil or method == "" or method == nil then
       error("Invalid arguments for save()", 2)
@@ -84,11 +106,14 @@
       return false
     end
   end
-  
-  -- Explode a string by separator (table response).
-  -- Parameters:
-  --  separator: explode at this character (required).
-  --  string: data to explode (required).
+
+  --[[
+      Explode a string by given separator.
+      
+      @param string separator The character to explode at (required)
+      @param string string The data to explode (required)
+      @returns table
+  ]]--
   function explode(separator, string)
     if separator == "" or separator == nil or string == "" or string == nil then
       error("Invalid arguments for explode()", 2)
@@ -103,11 +128,14 @@
       return table
     end
   end
-  
-  -- Implode a table with given separator (string response).
-  -- Parameters:
-  --  separator: implode with this character (required).
-  --  table: table to implode (required).
+
+  --[[
+      Implode a table with given separator.
+      
+      @param string separator The character to implode at (required)
+      @param table table The table to implode (required)
+      @returns string
+  ]]--
   function implode(separator, table)
     if type(table) ~= "table" or type(separator) ~= "string" or separator == "" then
       error("Invalid argument(s) for implode()", 2)
@@ -120,11 +148,13 @@
     end
   end
 
-  -- Wait for a keypress (boolean response).
-  -- If key is not defined, every key triggers the action.
-  -- Parameters:
-  --  key: the keypress we should wait for (optional).
-  --  exitonfalse: should the function return false if a keypress is not `key`? (optional).
+  --[[
+      Wait for a key press.
+      
+      @param string key The key to wait for to be pressed (optional)
+      @param boolean exitonfalse Should the function return false if a keypress is not 'key'? (optional)
+      @returns boolean
+  ]]--
   function keyPress(key, exitonfalse)
     if key == "" or key == nil or string.len(key) > 1 then
       key = "any"
@@ -145,13 +175,16 @@
       end
     end
   end
-  
-  -- Generate a random hash (data response).
-  -- Parameters:
-  --  length: the length of the random hash (required).
-  --  digits: use digits? (boolean) (optional).
-  --  upper: use uppercase letters? (boolean) (optional).
-  --  lower: use lowercase letters? (boolean) (optional).
+
+  --[[
+      Generate a random hash using a remote API.
+      
+      @param integer length The length of the random hash (required)
+      @param boolean digits Use digits in the hash? (optional)
+      @param boolean upper Use uppercase letters in the hash? (optional)
+      @param boolean lower Use lowercase letters in the hash? (optional)
+      @returns boolean/string Returns random generated hash or false on error.
+  ]]--
   function random(length, digits, upper, lower)
     if length == nil or length == "" then
       error("Invalid arguments for random()", 2)
@@ -174,10 +207,13 @@
     hash = load("http://www.random.org/strings/?num=1&format=plain&len="..length.."&digits="..digits.."&upperalpha="..upper.."&loweralpha="..lower.."", true)
     return hash
   end
-  
-  -- Dump a Table (data response).
-  -- Parameters:
-  --  table: the table to dump (required).
+
+  --[[
+      Make a table readable for debugging purposes.
+      
+      @param table table The table to dump (required)
+      @returns string
+  ]]--
   function dump(table)
     if table == nil or table == "" or type(table) ~= "table" then
       error("Invalid arguments for dump()", 2)
@@ -188,10 +224,13 @@
     end
     return result
   end
-  
-  -- Output all functions of given API.
-  -- Parameters:
-  --  api: the library/api to scan (required).
+
+  --[[
+      Get all the functions of the given API.
+      
+      @param table api The API to scan.
+      @returns string
+  ]]--
   function scanAPI(api)
     if api == nil or api == "" or type(api) ~= "table" then
       error("Invalid arguments for scanAPI()", 2)
@@ -206,8 +245,12 @@
     end
     return result
   end
-  
-  -- Output all the functions of Kit.
+
+  --[[
+      Display some information of Kit.
+      
+      @returns string
+  ]]--
   function help()
     result = kit.scanAPI(kit)
     result = kit.dump(result)
@@ -215,11 +258,15 @@
     term.clear()
     return "Welcome to Kit!\nVisit git.io/kitdocs for the documentation.\n\nHere's a list of all the functions you can use:\n"..result
   end
-  
-  -- Clear your screen and sets your cursor position (boolean response).
-  -- Parameters: 
-  --  screen: what screen you want to clear (required).
-  --  pos1, pos2: place you want your cursor to be at after the screen clears (optional).
+
+  --[[
+      Clear the given screen and set the cursor position.
+      
+      @param table screen The screen to clear (required)
+      @param integer pos1 Set the X position of the cursor (optional)
+      @param integer pos2 Set the Y position of the cursor (optional)
+      @returns boolean
+  ]]--
   function clear(screen, pos1, pos2)
     if screen == nil or screen == "" then
       error("Invalid arguments for clear()", 2)
@@ -232,11 +279,14 @@
     screen.setCursorPos(pos1, pos2)
     return true
   end
-  
-  -- Open a single or multiple modem ports (boolean response).
-  -- Parameters:
-  --  side: the side where your (wireless) modem is.
-  --  range: the range of ports we should open (max. is a total of 128), separated by a '-'.
+
+  --[[
+      Open modem port(s).
+      
+      @param string side The side where your wireless or wired modem is located (required)
+      @param string range The range of ports to open (max 128), separated by '-' (required)
+      @returns boolean
+  ]]--
   function open(side, range)
     if side == nil or side == "" or range == nil or range == "" then
       error("Invalid arguments for open()", 2)
@@ -254,11 +304,14 @@
     end
     return true
   end
-  
-  -- Close a single or multiple modem ports (boolean response).
-  -- Parameters:
-  --  side: the side where your (wireless) modem is.
-  --  range: the range of ports we should close (max. is a total of 128), separated by a '-'.
+
+  --[[
+      Close modem port(s).
+      
+      @param string side The side where your wireless or wired modem is located (required)
+      @param string range The range of ports to close (max 128), separated by '-' (required)
+      @returns boolean
+  ]]--
   function close(side, range)
     if side == nil or side == "" or range == nil or range == "" then
       error("Invalid arguments for close()", 2)
@@ -276,10 +329,13 @@
     end
     return true
   end
-  
-  -- Listen to opened port(s) (table response).
-  -- Parameters:
-  --  time: listen x seconds until breaking (optional).
+
+  --[[
+      Listen to opened port(s).
+      
+      @param integer timeout The amount of seconds to wait until breaking (optional)
+      @returns boolean/table Formatted received message or false on error.
+  ]]--
   function receive(timeout)
     if timeout == nil or timeout == "" then
     else
